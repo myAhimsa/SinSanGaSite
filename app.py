d67e3b3690df6d8d3bff38fabd9cd67a3c016add
myAhimsa <145898258+myAhimsa@users.noreply.github.com>
2025-07-17 09:29:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ users = {"user1": "password1", "user2": "password2"}
 
 def home():
     return render_template('home.html')
-    @app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 
-    def login():
-        if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
-            if username in users and users[username] == password:
-            return redirect(url_for('home'))
-        else:
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+    if username in users and users[username] == password:
+        return redirect(url_for('home'))
+    else:
         return "Невірний логін або пароль"
     return render_template('login.html')
 if __name__ == '__main__':
